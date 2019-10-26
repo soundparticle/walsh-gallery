979e0dadd514ffb6c8293c0de3d965269e9216c7
Antreo Pukay <acrosswalker@yahoo.com>
2019-10-26 12:44:28 -0700

    added thank you page/route

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <section>
+      <p>This app was created by Brooklynn Moore and Antreo Pukay</p>
+    </section>
   </div>
 </template>
---
+++ src/views/Thanks.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>This is the thank you page</h1>
+    <p>Thank you for signing up!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Thanks',
+};
+
+</script>
+
+<style scoped>
+
+</style>
